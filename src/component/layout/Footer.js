a7a0586efce93e6aef703232766990f85e06a403
mohamed mahmoud <m.mahmoud@iVAS.local>
2021-06-23 11:04:33 +0200

    handle menu

--- src/component/layout/Footer.js
+++ src/component/layout/Footer.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-export default function Footer () {
-    return (
-        <footer> <center>Created By Mohamed Mahmoud</center> </footer>
-    );
+export default function Footer() {
+  return (
+    <footer>
+      <center>Created By Mohamed Mahmoud</center>
+    </footer>
+  );
 }
